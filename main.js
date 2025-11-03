1c52afca68071fc77136f1a09bde7fe6956dad62
boboface <oumarbobo.diallo1@unchk.edu.sn>
2025-11-03 03:23:03 +0000

    Ajout de l'animation preloader

--- main.js
+++ main.js
@@ -1,38 +1,74 @@
-const tl = gsap.timeline({ defaults: { duration: 1 } });
-tl.from(".profiles-section", {
-    opacity: 0,
-    y: 70,
-    ease: "power2.out",
-    duration: 1.5
-});
-gsap.from("h2, p", {
-    opacity: 0,
-    y: 20,
-    duration: 1,
-    stagger: 0.2
-});
-gsap.from(".omar-socials a", {
-    opacity: 0,
-    x: -100,
-    duration: 1,
-    stagger: 0.2
-});
-gsap.from(".modibo-socials a", {
-    opacity: 0,
-    x: 100,
-    duration: 1,
-    stagger: 0.2
-});
-gsap.from(".middle-infinity", {
-    opacity: 0,
-    y: -50,
-    duration: 1
-});
-gsap.from(".infinity", {
-    opacity: 0,
-    scale: 0,
-    duration: 1,
-    ease: "elastic.out(1, 0.5)",
-    delay: 0.5
-});
+// gsap.registerPlugin(SplitText);
 
+CustomEase.create("hop", "0.9, 0, 0.1, 1");
+
+gsap.set([".preloader-header a", ".preloader-copy p"], { autoAlpha: 0, y: 20 });
+
+const preloaderImages= gsap.utils.toArray('.preloader-images .img');
+const preloaderImagesInner= gsap.utils.toArray('.preloader-images img');
+
+const tl = gsap.timeline({ delay: 0.25});
+
+tl.to(".progress-bar",{
+    scaleX: 1,
+    duration: 5,
+    ease: "power3.inOut"
+})
+.set(".progress-bar",{ transformOrigin: "right"})
+.to(".progress-bar",{
+    scaleX: 0,
+    duration: 1,
+    ease: "power3.in"
+})
+
+preloaderImages.forEach((preloaderImg, index) => {
+    tl.to(preloaderImg, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 1.8, // Increased from 1.5
+        delay: index * 0.75,
+    }, "-=5");  
+})
+
+preloaderImagesInner.forEach((preloaderImagesInner, index) => {
+    tl.to(preloaderImagesInner, {
+        scale: 1.1,
+        duration: 2.4, // Increased from 2
+        ease: "power3.inOut",
+        delay: index * 0.75,
+    }, "-=5.25");  
+})
+
+tl.to([".preloader-header a", ".preloader-copy p"], {
+    autoAlpha: 1,
+    y: 0,
+    duration: 1.5,
+    ease: "power3.out",
+    stagger: 0.2
+}, "-=5.5");
+
+tl.to(
+    ".preloader-images",{
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        duration: 1.5,
+        ease: "power3.inOut",
+    },
+    "-=1.5"
+);
+
+tl.to(
+    ".preloader", {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        duration: 2,
+        ease: "power3.inOut",
+    },
+    "-=0.5"
+);
+
+tl.to(
+    ".preloader-header", {
+        autoAlpha: 0,
+        duration: 1.5,
+        ease: "power3.inOut"
+    },
+    "<"
+);
